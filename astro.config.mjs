a89fb7079d0f835585a0007964da19c06d00d89c
Mayank <9084735+mayank99@users.noreply.github.com>
2022-06-07 20:39:00 +0000

    update netlify adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
 import { defineConfig } from 'astro/config';
 import preact from '@astrojs/preact';
-import netlify from "@astrojs/netlify/edge-functions";
+import netlify from '@astrojs/netlify/edge-functions';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [preact()],
-	adapter: netlify()
+  adapter: netlify(),
+  // vite: { ssr: { noExternal: false } },
 });
